b159779b93ba94f75b64f79e2dcd11d406e77c2d
SuycxZMZ <suycx65@163.com>
2024-06-02 20:06:01 +0800

    modified:   test/test_asynclog.cc

--- test/test_asynclog.cc
+++ test/test_asynclog.cc
@@ -3,7 +3,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <thread>
+#include <chrono>
+#include <filesystem>
 
+namespace fs = std::filesystem;
+
+// 计算时间
+class TimeLogger {
+public:
+    TimeLogger() {
+        time1 = std::chrono::high_resolution_clock::now(); // 构造时记录开始时间
+    }
+
+    ~TimeLogger() {
+        time2 = std::chrono::high_resolution_clock::now(); // 析构时记录结束时间
+        std::chrono::duration<double> duration = time2 - time1;
+        // 计算当前文件夹下所有 .log 文件的总大小  
+        size_t total_size = 0;
+        for (const auto& entry : fs::directory_iterator(".")) {
+            if (entry.is_regular_file() && entry.path().extension() == ".log") {
+                total_size += entry.file_size();
+            }
+        }
+        
+        double rate = total_size / duration.count();
+        printf("Total log size: %zu bytes\n", total_size);
+        printf("Logging 1000000 entries took %f seconds\n", duration.count());
+        printf("Rate: %.6f bytes/second\n", rate);
+    }
+public:
+    std::chrono::high_resolution_clock::time_point time1;
+    std::chrono::high_resolution_clock::time_point time2;
+};
 
 void test_SyncLogging()
 {
@@ -27,6 +58,8 @@ void test_AsyncLogging()
     }
 }
 
+TimeLogger timeLogger;
+
 int main(int argc, char* argv[])
 {
     printf("pid = %d\n", getpid());
@@ -34,6 +67,8 @@ int main(int argc, char* argv[])
 
     symlog::initAsyncLogging(::basename(argv[0]), 1024 * 1024 * 50);
     symlog::AsyncLogStart();
+
+    timeLogger.time1 = std::chrono::high_resolution_clock::now();
 
     std::thread t1(test_AsyncLogging);
     std::thread t2(test_AsyncLogging);
